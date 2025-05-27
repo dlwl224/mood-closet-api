7f9187985d649119e50c02ff28e68e2dd0042904
dlwl224 <jw5194@naver.com>
2025-05-27 21:53:27 +0900

    중복처리

--- api/ootdAI.js
+++ api/ootdAI.js
@@ -8,9 +8,8 @@ const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 export default async function handler(req, res) {
   
-  const allowedOrigin="https://dlwl224.github.io"
+  const allowedOrigin="https://dlwl224.github.io";
   res.setHeader("Access-Control-Allow-Origin", allowedOrigin);
-  res.setHeader("Access-Control-Allow-Origin", "");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   if (req.method === "OPTIONS") {
